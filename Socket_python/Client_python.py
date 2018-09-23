d2cbbf43fb0357b24513275a9a063090635d1d81
gabrielrb1 <32144610+gabrielrb1@users.noreply.github.com>
2018-09-23 16:13:45 -0300

    Add files via upload

--- Socket_python/Client_python.py
+++ Socket_python/Client_python.py
@@ -3,23 +3,46 @@
 import socket
 import timeit
 
-protocolo = input("Indicar o protocolo de transporte desejado (UTP/TCP)")
-tentativas = input("Indique a quantidade de tentativas")
-
-TCP_IP = '127.0.0.1'
-TCP_PORT = 5005
-BUFFER_SIZE = 1024
+protocolo = input("Indicar o protocolo de transporte desejado (UTP/TCP) ")
+tentativas = int (input("Indique a quantidade de tentativas "))
+IP = '127.0.0.1'
+PORT = 5005
 MESSAGE = "Hello"
+n = 0
+media = 0
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((TCP_IP, TCP_PORT))
-s.send(MESSAGE.encode("utf-8"))
-start = timeit.timeit()
-data = s.recv(BUFFER_SIZE)
-s.close()
-end = timeit.timeit()
-elapsed = end-start
+if protocolo == "TCP":
+    print ("TCP selecionado")
+    while (tentativas>0):
+        TCP_IP = IP
+        TCP_PORT = PORT
+        BUFFER_SIZE = 1024
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((TCP_IP, TCP_PORT))
+        s.send(MESSAGE.encode("utf-8"))
+        start = timeit.timeit()
+        data = s.recv(BUFFER_SIZE)
+        s.close()
+        end = timeit.timeit()
+        elapsed = end-start
+        print (elapsed)
+        tentativas = tentativas - 1
 
-#print ("received data:", data)
-print (elapsed)
-print (protocolo)
+elif protocolo == "UDP":
+    print("UDP selecionado")
+    while (tentativas>0):
+        UDP_IP = IP
+        UDP_PORT = PORT
+        sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+        sock.sendto(MESSAGE.encode("utf-8"), (UDP_IP, UDP_PORT))
+        start = timeit.timeit()
+        end = timeit.timeit()
+        elapsed = end-start
+        print (elapsed)
+        tentativas = tentativas - 1
+        n = n + 1
+        media = (media + elapsed)/n
+    print("Tempo médio de resposta: ",media)
+
+else:
+    print ("Selecione um protocolo válido")
